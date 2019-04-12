93d12b1aa37676b27962df82bb1261f139fd06cd
Victor Petrescu <victor@districtm.net>
2019-04-11 21:05:48 -0400

    problem08solved

--- problems/problem8.js
+++ problems/problem8.js
@@ -1,9 +1,9 @@
 let verifyEquals = require('./verify-equals.js');
 
 // we need 5 test cases.
-let inputs = [];
+let inputs = ['hello', 'bye', 'this', 'is', 'reversed'];
 
-let outputs = [];
+let outputs = ['olleh', 'eyb', 'siht', 'si', 'desrever'];
 
 /*
 Make this function return the input string, reversed. For example "hello" would return "olleh" and "how are you" would return "uoy era woh".
@@ -15,7 +15,16 @@ HINTS:
  - Loop over the array of split characters in reverse order and fill the empty array by pushing each character into it
  - Convert the filled array into a string (use the join method) and return it
 */
-function f(str) {}
+function f(str) {
+  let reverseArray = [];
+  let charArray = str.split('')
+  for(let i = 0; i < str.length; i++){
+    reverseArray.push(charArray.pop())
+  }
+  reverseArray = reverseArray.join('')
+  console.log(reverseArray)
+  return reverseArray
+}
 
 //This function runs a test. You do not need to change any code under here
 function runTest(i) {
